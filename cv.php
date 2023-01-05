21978bcd754a8633b5245f825bd4a259fbf5df0a
MacBook <macbook@MacBooks-MacBook-Pro-2.local>
2023-01-05 09:52:16 +0100

    Ajout d'un routeur

--- cv.php
+++ cv.php
@@ -9,13 +9,13 @@
 <?php
     include('components/header4janvier.php');
 ?>
-
-<div class="linkpdf">
-    <a href="cvStage-whiteco.pdf" class="flex justify-center text-md underline w-full">Cliquez ici pour ouvrir le pdf dans un nouvel onglet</a>
+<br>
+<div class="block md:hidden">
+    <a href="cvStage-whiteco.pdf" class="flex justify-center text-md underline w-full">(mobile) cliquez ici pour ouvrir le pdf dans un nouvel onglet</a>
 </div>
-
+<br>
 <div class="relative">
-    <div class="flex justify-center" > <!--  style="background-color: #262c43" -->
+    <div class="flex justify-center"> <!--  style="background-color: #262c43"  height="900px" width="600px" -->
         <object data="cvStage-whiteco.pdf" type="application/pdf" height="900px" width="600px">
             <p class="text-white"><u>Votre navigateur ne prend pas en charge l'affichage de PDF intégrés. Vous pouvez <a href="cvStage-whiteco.pdf">télécharger le fichier PDF ici</a> pour le visualiser.</u></p>
         </object>
